fc56e61ff09130cb6b9063fc08ea102415411e95
Ibraheem-Aredda <aredda.ibrahim@gmail.com>
2021-05-22 17:49:56 +0100

    Upload

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
-include_once "templates/template.php";
-include_once "src/utilities/handler.php";
+echo "good";
 
-Template::useTemplate("مرحبا بكم في أردروس", "views/catalog.php");
+// include_once "templates/template.php";
+// include_once "src/utilities/handler.php";
+
+// Template::useTemplate("مرحبا بكم في أردروس", "views/catalog.php");
