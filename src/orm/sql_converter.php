98a19db18e33b74ade9d4e6dfdb033023aa77836
Ibraheem-Aredda <aredda.ibrahim@gmail.com>
2021-05-23 13:39:43 +0100

    Logging

--- src/orm/sql_converter.php
+++ src/orm/sql_converter.php
@@ -180,6 +180,13 @@ class SQLConverter
         $connection->query ("SET NAMES utf8");
         // Query result
         $result = $connection->query($query);
+
+        if(!$result)
+        {
+            echo $connection->error;
+            return;
+        }
+
         // Add to the empty array
         while ($row = $result->fetch_assoc())
             array_push($data, $row);
